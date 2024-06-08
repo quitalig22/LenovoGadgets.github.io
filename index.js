cfd2398553eb2005529883d959c088805c6514bf
quitalig22 <quitaligsheryl@gmail.com>
2024-06-08 12:02:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ const commentbox = document.getElementById("commentbox")
 function submitComment(){
 
 commentbox.textContent += comment.value.toString() + "\n"
-  comment.value =" "; 
+comment.value = " ";
 }
 submit.addEventListener("click", submitComment)
